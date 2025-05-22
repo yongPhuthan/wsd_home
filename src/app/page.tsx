eae889c1b705c8550369dce12a945e8f9702e007
yongPhuthan <86427357+yongPhuthan@users.noreply.github.com>
2025-05-22 16:22:29 +0700

    first commit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -12,7 +12,7 @@ export default function HomePage() {
       <HeroSection />
       <KeyFeaturesSection />
         <WhyWorkStandardSection />
-      <PricingSection />
+      {/* <PricingSection /> */}
       {/* <TestimonialsSection /> */}
       <FaqSection />
     </>
